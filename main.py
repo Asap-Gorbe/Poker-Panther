7096d11432e65dfa5d57f955d6ebb8b3b38a672e
Asap-Gorbe <dmb_plata@outlook.com>
2025-03-01 23:01:25 +0330

    made it clean and refactor

--- main.py
+++ main.py
@@ -1,58 +1,44 @@
 import random
 
-cards = []
-suits = ["Heart", "Diamond", "Club", "Spade"]
-ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+from Bastion import account
 
-for suit in suits:
-    for rank in ranks:
-        cards.append(f"{rank}-{suit}")
+
+class Deck:
+    def __init__(self):
+        self.Deck = []
+
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+        for rank in ranks:
+            for suit in suits:
+                self.Deck.append(f"{rank}-{suit}")
+
+    def deal(self):
+        random.shuffle(self.Deck)
+        card_1_2 = self.Deck[-2:]
+        del self.Deck[-2:]
+        return card_1_2
+
 
 class Player:
-    def __init__(self, username):
+    def __init__(self, username,Deck,Chip=1000):
         self.username = username
-        self.chips = 1000
-        self.isFold = False
-        self.cards = []
-    
-    def check(self):
-        pass
-    
-    def bet(self, chip_count):
-        pass
-
-    def fold(self):
-        self.isFold = True
+        self.Deck = Deck
+        self.Chip = Chip
+    def action (self):
+        action = False
+    def bet (self,amount):
+        action = True
+        self.Chip -= amount
+        if amount > self.Chip :
+            print('you cant bet more than your balance ')
 
 
-class PokerGame:
-    def __init__(self):
-        self.players = []
-        self.board = []
-        self.min_bet = 10
-    
-    def start(self):
-        self.deal()
-        self.board.append(cards[-3:])
-        del cards[-3:]
-    
-    def deal(self):
-        random.shuffle(cards)
-        for player in self.players:
-            player.cards = cards[-2:]
-            del cards[-2:]
-    
-    def add_player(self, player):
-        self.players.append(player)
 
 
-# --- Main App ---
 
-if __name__ == "__main__":
-    game = PokerGame()
-    game.add_player(Player("shayan"))
-    game.add_player(Player("mamad"))
-    game.start()
-    
-    for player in game.players:
-        print(player.cards)
+
+cards = Deck()
+p1 = Player('A$AP Gorbe',Deck.deal())
+p2 = Player("shayanstx", Deck.deal())
